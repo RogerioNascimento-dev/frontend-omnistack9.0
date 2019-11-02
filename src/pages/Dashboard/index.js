8670aeac712692b46b0422a343ab488b2fef17ce
RogerioNascimento-dev <rogerionascimento.dev@outlook.com.br>
2019-11-01 23:11:44 -0300

    rota spots concluida

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,21 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
-
+import { Link } from 'react-router-dom';
+import './style.css';
 export default function Dashboard(){
+  const [spots, setSpots] = useState([]);
+
   useEffect(() =>{
     async function carregarSpots(){
       const user_id = localStorage.getItem('user');
       const returno = await api.get('/dashboard',{
         headers: {user_id}
-      });
-
-      console.log(returno);
+      });   
+      
+      setSpots(returno.data);
     }
     carregarSpots();
   },[]);
 
   return (
     <>
-    <h1>Estou no dashboard</h1>
+      <ul className="spot-list">
+        {spots.map(spot => (
+          <li key={spot._id}>
+            <header style={{backgroundImage: `url(${spot.imagem_url})`}}/>
+            <strong>{spot.empresa}</strong>
+            <span>{spot.valor ? `R$${spot.valor}/dia`:'Gratuito'}</span>
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/novo">
+        <button className="btn">
+        Cadastrar novo spot
+        </button>
+        </Link>
     </>
   )}
