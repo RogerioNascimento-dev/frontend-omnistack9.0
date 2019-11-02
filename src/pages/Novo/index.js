17caf4518c28292e53c28e91dd2a176adb2edf65
RogerioNascimento-dev <rogerionascimento.dev@outlook.com.br>
2019-11-02 00:16:26 -0300

    finalizando aplicação web com react

--- src/pages/Novo/index.js
+++ src/pages/Novo/index.js
@@ -1,8 +1,77 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import api from '../../services/api';
 
-export default function Novo(){
+import './styles.css';
+import camera from '../../assets/camera.svg';
+
+
+export default function Novo({history}){
+  const [empresa, setEmpresa] = useState('');
+  const [valor, setValor] = useState('');
+  const [tecnologias, setTecnologias] = useState('');
+  const [imagem, setImagem] = useState(null);
+
+  const preview = useMemo(() => {
+    return imagem ? URL.createObjectURL(imagem): null;
+  },[imagem])
+
+
+  async function handleSubmit(event){
+    event.preventDefault();
+    const data = new FormData();
+    const user_id = localStorage.getItem('user');
+    data.append('imagem',imagem);
+    data.append('empresa',empresa);
+    data.append('valor',valor);
+    data.append('tecnologias',tecnologias);
+
+    
+    await api.post('/spots', data,{
+      headers: {user_id}
+    });
+
+    history.push('/dashboard');
+  }
+
   return (
     <>
-    <h1>Estou no novo</h1>
+    <form onSubmit={handleSubmit}>
+      <label 
+      id="imagem"
+      style={{backgroundImage: `url(${preview})`}}
+      className={imagem ? 'has-imagem':''}
+      >
+        <input type="file" onChange={event => setImagem(event.target.files[0])}/>
+        <img src={camera} alt="Selecione a imagem" />
+      </label>
+      <label htmlFor="empresa">Empresa *</label>
+      <input
+      id="empresa"
+      placeholder="Nome da empresa"
+      value={empresa}
+      onChange={event => setEmpresa(event.target.value)}
+      />
+
+<label htmlFor="tecnologias">Tecnologias <span>(Separado por vírgula)</span></label>
+      <input 
+      type="text"
+      id="tecnologias"
+      placeholder="Quais tecnologias usam?"
+      value={tecnologias}
+      onChange={event => setTecnologias(event.target.value)}
+      />
+
+      <label htmlFor="valor">Valor <span>(Em branco se gratuito)</span></label>
+      <input
+      type="text"
+      id="valor"
+      placeholder="Nome da valor"
+      value={valor}
+      onChange={event => setValor(event.target.value)}
+      />
+
+      
+  <button type="submit" className="btn">Cadastrar</button>
+    </form>
     </>
   )}
